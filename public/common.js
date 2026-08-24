0a992a08516fc92b3151604c7d88d167204a9605
kxlynn666 <kxlynn666@gmail.com>
2026-08-23 21:10:27 -0300

    Load responsive UI v3 across shared pages

--- public/common.js
+++ public/common.js
@@ -83,6 +83,22 @@ window.SkyNet = (() => {
 })();
 
 (() => {
+    if (!document.querySelector('link[data-skynet-ui-v3]')) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = '/ui-v3.css';
+        link.dataset.skynetUiV3 = '1';
+        document.head.appendChild(link);
+    }
+    if (!document.querySelector('script[data-skynet-site-ui-v3]')) {
+        const script = document.createElement('script');
+        script.src = '/site-ui-v3.js';
+        script.dataset.skynetSiteUiV3 = '1';
+        document.head.appendChild(script);
+    }
+})();
+
+(() => {
     const cleanPath = location.pathname.replace(/\/+$/, '') || '/';
 
     if (cleanPath.startsWith('/u/')) {
